7f06c2b6cb15695b81afb43c030b48b68ee147f3
prakhar <prakharharshit.2003@gmail.com>
2024-06-06 10:32:58 +0530

    adding functionality in app.jsx

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,15 +1,52 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import {login,logout} from "./store/authSlice";
+import {Header,Footer} from "./components/index"
+import {Outlet} from 'react-router-dom'
+//dispatch is a merger of react and redux
+import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  // .finally issliye use kiya authservice run hoga hi hoga chaye .then ho ya .catch
+  useEffect(() => {
+    authService.getCurrentUser()
+    //then callback mai hme Data milta hai 
+    //we collect data from user if we get then we login() else(if we dont get data) we logout().we use logout beacause it will update state if userdata dont find  
+    .then((userData)=>{
+    if(userData){
+      dispatch(login({
+        userData
+      }))
+    }
+    else{
+      dispatch(logout())
+    }
+    })
 
-  return (
-    <>
-      <h1>Blog Website using Appwrite</h1>
-    </>
-  )
+    .finally(()=>setLoading(false));
+  }, []);
+  //return in condional renderring
+ return !loading ? (
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+    <div className="w-full block">
+<Header />
+  <main>
+    <Outlet/>   
+    //outlet comes from react router 
+  </main>
+<Footer />
+    </div>
+  </div>
+ ) : null
+
+  // return (
+  //   <>
+  //     <h1>Blog Website using Appwrite</h1>
+  //   </>
+  // );
 }
 
-
-export default App
+export default App;
---
+++ Blog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Footer/Footer";
+import Footer from "./Header/Header";
+
+export {
+    Header,
+    Footer
+}
