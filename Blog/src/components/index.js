e82b0036db1a1f26efde2dd461d38e24a18fbffd
prakhar <prakharharshit.2003@gmail.com>
2024-06-06 17:13:53 +0530

    adding different components and use of Forwardref

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Footer/Footer";
 import Footer from "./Header/Header";
-
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/Logoutbtn'
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+
 }
