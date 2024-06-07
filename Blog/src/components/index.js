1db1bdf3a722843fb78516f28edcdba49d8fa1d6
prakhar <prakharharshit.2003@gmail.com>
2024-06-07 17:56:55 +0530

    update Postform and RTE

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Header/Header";
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header/Logoutbtn'
+import RTE from './RTE'
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 
 }
